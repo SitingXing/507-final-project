1a11eeeb9d43319db636d87263dc0dd6de762b15
SitingXing <121953533+SitingXing@users.noreply.github.com>
2023-04-10 17:15:01 +0000

    Fix bugs on tree construction

--- tree.py
+++ tree.py
@@ -32,8 +32,12 @@ def construct_state_tree(state_info, coal_data, elec_data, gas_data, temp_data, 
     for year in range(2015, 2023):
         for month in ['Q1', 'Q2', 'Q3', 'Q4']:
             month_node = energy.children[0].add_child(str(year) + '-' + month)
-            month_node.add_child(coal_data['data'][coal_index]['value'])
-            coal_index -= 1
+            try:
+                if month_node.name == coal_data['data'][coal_index]['time']:
+                    month_node.add_child(coal_data['data'][coal_index]['value'])
+                    coal_index -= 1
+            except IndexError:
+                break
 
     for type in ['electricity', 'natural gas']:
         if type == 'electricity':
@@ -47,8 +51,12 @@ def construct_state_tree(state_info, coal_data, elec_data, gas_data, temp_data, 
         for year in range(2015, 2023):
             for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
                 month_node = energy.children[index].add_child(str(year) + '-' + month)
-                month_node.add_child(data['data'][eg_index]['value'])
-                eg_index -= 1
+                try:
+                    if month_node.name == data['data'][index]['date']:
+                        month_node.add_child(data['data'][eg_index]['value'])
+                        eg_index -= 1
+                except IndexError:
+                    break
 
     for weather_type in ['temperature', 'precipitation', 'wind']:
         weather_node = weather.add_child(weather_type)
@@ -64,36 +72,37 @@ def construct_state_tree(state_info, coal_data, elec_data, gas_data, temp_data, 
         for year in range(2015, 2023):
             for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
                 month_node = weather_node.add_child(str(year) + '-' + month)
-                month_node.add_child(weather_data['data'][index]['value'])
-                index += 1
+                try:
+                    if month_node.name == weather_data['data'][index]['date']:
+                        month_node.add_child(weather_data['data'][index]['value'])
+                        index += 1
+                except IndexError:
+                    break
 
     return state
 
 
+def get_data_from_cache(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+
 def main():
-    with open('state info.json', 'r') as file:
-        states_info = json.load(file)[0]
+    states_info = get_data_from_cache('state info.json')
+    coal_data = get_data_from_cache('energy_data_cache/coal_data.json')
+    electricity_data = get_data_from_cache('energy_data_cache/electricity_data.json')
+    natural_gas_data = get_data_from_cache('energy_data_cache/natural_gas_data.json')
+    temperature_data = get_data_from_cache('weather_data_cache/temperature_data.json')
+    precipitation_data = get_data_from_cache('weather_data_cache/precipitation_data.json')
+    wind_data = get_data_from_cache('weather_data_cache/wind_data.json')
 
-    with open('energy_data_cache/coal_data.json', 'r') as file:
-        coal_data = json.load(file)[0]
+    root = Node('USA')
 
-    with open('energy_data_cache/electricity_data.json', 'r') as file:
-        electricity_data = json.load(file)[0]
-
-    with open('energy_data_cache/natural_gas_data.json', 'r') as file:
-        natural_gas_data = json.load(file)[0]
-
-    with open('weather_data_cache/temperature_data.json', 'r') as file:
-        temp_data = json.load(file)[0]
-
-    with open('weather_data_cache/precipitation_data.json', 'r') as file:
-        prec_data = json.load(file)[0]
-
-    with open('weather_data_cache/wind_data.json', 'r') as file:
-        wind_data = json.load(file)[0]
-
-    state_test = construct_state_tree(states_info, coal_data, electricity_data, natural_gas_data, temp_data, prec_data, wind_data)
-
+    for i in range(50):
+        state_tree = construct_state_tree(states_info[i], coal_data[i], electricity_data[i], natural_gas_data[i], temperature_data[i], precipitation_data[i], wind_data[i])
+        root.children.append(state_tree)
 
 
 if __name__ == '__main__':
